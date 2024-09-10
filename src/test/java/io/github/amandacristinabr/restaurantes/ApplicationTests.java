3b43652b7418100b3f8970825aea6376fe5c6658
Amanda Cristina <amandadaylan@gmail.com>
2024-09-10 17:37:46 -0300

    iniciando projeto

---
+++ src/test/java/io/github/amandacristinabr/restaurantes/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.amandacristinabr.restaurantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
